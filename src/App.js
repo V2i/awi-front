f5e359b69d1c7b113c3edf2e1fd2480856d3f367
Valentin Guyon <v.guyon@fraudio.com>
2021-03-24 09:19:51 +0100

    FIX: navbar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Game from "./containers/Game";
 
 import EditorList from "./containers/EditorList";
 import Editor from "./containers/Editor";
+
 import ExhibitorList from "./containers/ExhibitorList";
 import Exhibitor from "./containers/Exhibitor";
 
@@ -15,27 +16,24 @@ function App() {
   return (
     <div className="App">
         <nav>
-            <NavLink to={"/festival/list"}>
-                Festival
-            </NavLink>
-            <br />
-            <NavLink to={"/game/list"}>
-                Jeux
-            </NavLink>
+            <NavLink to={"/festival/list"}>Festivals</NavLink>
+            <NavLink to={"/game/list"}>Jeux</NavLink>
+            <NavLink to={"/editor/list"}>Editeurs</NavLink>
+            <NavLink to={"/exhibitor/list"}>Exposants</NavLink>
         </nav>
       <Switch>
         <Route path={"/festival/list"} exact component={FestivalList}/>
         <Route path={"/festival/:id"} exact component={Festival}/>
 
-
         <Route path={"/game/list"} exact component={GameList}/>
         <Route path={"/game/:id"} exact component={Game}/>
 
-
         <Route path={"/editor/list"} exact component={EditorList}/>
         <Route path={"/editor/:id"} exact component={Editor}/>
+
         <Route path={"/exhibitor/list"} exact component={ExhibitorList}/>
         <Route path={"/exhibitor/:id"} exact component={Exhibitor}/>
+
         <Redirect to={"/"}/>
       </Switch>
     </div>
