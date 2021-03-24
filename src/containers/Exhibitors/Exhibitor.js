17c1921ec0e98199f1ef6dd5518a0ae426b43c94
Alexiaognard <ognardalexiats4@gmail.com>
2021-03-24 17:32:37 +0100

    ADD : CSS for Exhibitor

---
+++ src/containers/Exhibitors/Exhibitor.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {getExhibitorByID} from "../../actions/ExhibitorActions";
+import _ from "lodash";
+import Loading from "../Loading";
+import { 
+    Table, TableBody, TableCell, TableRow, TableHead, TableContainer,
+    Paper, Button, Typography, Grid,
+  } from "@material-ui/core";
+
+const Exhibitor = (props) => {
+
+    const exhibitorId = props.match.params.id;
+    const dispatch = useDispatch();
+    const exhibitor = useSelector(state => state.Exhibitor);
+
+    React.useEffect(() => {
+        dispatch(getExhibitorByID(exhibitorId));
+    }, [dispatch, exhibitorId]);
+
+    const showData = () => {
+        if(!_.isEmpty(exhibitor.data)) {
+            return (
+                <Paper>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <Typography variant="h3">{exhibitor.data.exhibitorName}</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Typography>Relié à un éditeur ? {exhibitor.data.exhibitorEditor ? <Button variant="outlined" href={`/editor/${exhibitor.data.exhibitorEditor._id}`}>{exhibitor.data.exhibitorEditor.editorName}</Button> : 'Non'}</Typography>
+                        </Grid>
+                        
+                    </Grid>
+                    
+                    <Typography><b>Contacts :</b></Typography>
+                    <TableContainer component={Paper}>
+                        <Table size="small" aria-label="simple table">
+                            <TableHead>
+                            <TableRow>
+                                <TableCell>Nom</TableCell>
+                                <TableCell>Prénom</TableCell>
+                                <TableCell>Téléphone 1</TableCell>
+                                <TableCell>Téléphone 2</TableCell>
+                                <TableCell>Mail</TableCell>
+                                <TableCell>Fonction</TableCell>
+                                <TableCell>Principal ?</TableCell>
+                            </TableRow>
+                            </TableHead>
+                            <TableBody>
+                            {exhibitor.data.exhibitorContact && exhibitor.data.exhibitorContact.map(row => (
+                                <TableRow key={row._id}>
+                                    <TableCell component="th" scope="row">{row.contactLastname}</TableCell>
+                                    <TableCell component="th" scope="row">{row.contactFirstname}</TableCell>
+                                    <TableCell component="th" scope="row">{row.contactPhone}</TableCell>
+                                    <TableCell component="th" scope="row">{row.contactMobilePhone}</TableCell>
+                                    <TableCell component="th" scope="row">{row.contactMail}</TableCell>
+                                    <TableCell component="th" scope="row">{row.contactFunction}</TableCell>
+                                    <TableCell component="th" scope="row">{row.contactMain ? 'Oui' : 'Non'}</TableCell>
+                                </TableRow>
+                            ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Paper>
+            )
+        }
+        if(exhibitor.loading) {
+            return <Loading color={'lightblue'} type={'bubbles'} />
+        }
+        if(exhibitor.errorMsg !== "") {
+            return <p>{exhibitor.errorMsg}</p>;
+        }
+
+        return <p>unable to get data</p>;
+    };
+
+    return(
+        <div>
+            {showData()}
+        </div>
+    )
+}
+
+export default Exhibitor;
---
+++ src/containers/Exhibitors/ExhibitorList.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import _ from 'lodash';
+import Loading from "../Loading";
+import {getExhibitorList} from "../../actions/ExhibitorActions";
+import { 
+    Table, TableBody, TableCell, TableRow, TableHead, TableContainer,
+    Paper, Button
+  } from "@material-ui/core";
+
+const ExhibitorList = () => {
+
+    const dispatch = useDispatch();
+    const exhibitorList = useSelector(state => state.ExhibitorList);
+
+    React.useEffect(() => {
+        const fetchData = () => {
+            dispatch(getExhibitorList());
+        };
+        fetchData();
+    }, [dispatch]);
+
+    const showData = () => {
+        if(!_.isEmpty(exhibitorList.data)) {
+            return (
+                <TableContainer component={Paper}>
+                    <Table size="small" aria-label="simple table">
+                        <TableHead>
+                        <TableRow>
+                            <TableCell>Nom</TableCell>
+                            <TableCell align="right">Est Editeur ?</TableCell>
+                            <TableCell> </TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {exhibitorList.data.map(row => (
+                            <TableRow key={row._id}>
+                                <TableCell component="th" scope="row">
+                                    {row.exhibitorName}
+                                </TableCell>
+                                <TableCell align="right">{row.exhibitorEditor ? <Button  variant="outlined" href={`/editor/${row.exhibitorEditor._id}`}>{row.exhibitorEditor.editorName}</Button>: 'Non'}</TableCell>
+                                <TableCell><Button href={`/exhibitor/${row._id}`}>Détails</Button></TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            )
+        }
+        if(exhibitorList.loading) {
+            return <Loading color={'lightblue'} type={'bubbles'} />;
+        }
+        if(exhibitorList.errorMsg !== "") {
+            return <p>{exhibitorList.errorMsg}</p>;
+        }
+
+        return <p>unable to get data</p>;
+    };
+
+    return(
+        <div>
+            <h1>Liste des Exposants</h1>
+            {showData()}
+        </div>
+    )
+}
+
+export default ExhibitorList;
