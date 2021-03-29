2fcdc9e03e14e00bc60c9f2754e7d593ab9c8f8a
Valentin Guyon <v.guyon@fraudio.com>
2021-03-29 22:01:46 +0200

    FIX: view for guest v2.0

--- src/containers/Editors/Editor.js
+++ src/containers/Editors/Editor.js
@@ -59,77 +59,106 @@ const Editor = (props) => {
 
     const showData = () => {
         if(!_.isEmpty(editor.data)) {
-            return (
-                <Grid container spacing={3}>
-                    <Grid item xs={12}><h1>{editor.data.editorName}</h1></Grid>
-                    { toUpdate &&
-                    <Grid item xs={4}>
-                        <TextField name="editorName" label="Nom" value={editorSelected.editorName} onChange={handleChange}/>
-                        <Button onClick={() => updateEditor(editorSelected)}>Modifier</Button>
-                    </Grid>
-                    
-                    }
-                    { !toUpdate && 
+            if(user.isLoggedIn) {
+                return(
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}><h1>{editor.data.editorName}</h1></Grid>
+
+                        {toUpdate &&
+                        <Grid item xs={4}>
+                            <TextField name="editorName" label="Nom" value={editorSelected.editorName}
+                                       onChange={handleChange}/>
+                            <Button onClick={() => updateEditor(editorSelected)}>Modifier</Button>
+                        </Grid>
+                        }
+                        {!toUpdate &&
                         <Grid item xs={4}>
                             <Button onClick={() => setUpdate(true)}>Modifier</Button>
-                        </Grid>
-                    }
+                        </Grid>}
 
-                    {user.isLoggedIn
-                        ?
-                            <Grid item xs={4}>
-                                <Button onClick={() => removeEditor(editorId)}>Supprimer</Button>
-                            </Grid>
-                        :
-                            <></>
-                    }
-                    <Grid item xs={9}>
-                        <Typography><h3>Liste des jeux</h3></Typography>
-                        {user.isLoggedIn
-                            ? <IconButton variant="outlined" color="primary" onClick={() => changeValueOpen(true)}><Add /></IconButton>
-                            : <></>
-                        }
-                        <Table aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Nom</TableCell>
-                                    <TableCell>Age Min</TableCell>
-                                    <TableCell>Durée (en min)</TableCell>
-                                    <TableCell>Min Joueurs</TableCell>
-                                    <TableCell>Max Joueurs</TableCell>
-                                    <TableCell>Catégorie</TableCell>
-                                    <TableCell>Notice</TableCell>
-                                    <TableCell>Prototype ?</TableCell>
-                                    <TableCell> </TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                            {gameList.data.map((row) => (
-                                <TableRow key={row._id}>
-                                    <TableCell>{row.gameName}</TableCell>
-                                    <TableCell>{row.gameMinimumAge}</TableCell>
-                                    <TableCell>{row.gameDuration}</TableCell>
-                                    <TableCell>{row.gameMinimumPlayers}</TableCell>
-                                    <TableCell>{row.gameMaximumPlayers}</TableCell>
-                                    <TableCell>{row.gameType.gameTypeName}</TableCell>
-                                    <TableCell>{row.gameNotice}</TableCell>
-                                    <TableCell>{row.isPrototype ? 'Oui' : 'Non'}</TableCell>
-                                    {user.isLoggedIn
-                                        ?
+                        <Grid item xs={4}>
+                            <Button onClick={() => removeEditor(editorId)}>Supprimer</Button>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <Typography><h3>Liste des jeux</h3></Typography>
+                            <IconButton variant="outlined" color="primary" onClick={() => changeValueOpen(true)}><Add /></IconButton>
+                            <Table aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Nom</TableCell>
+                                        <TableCell>Age Min</TableCell>
+                                        <TableCell>Durée (en min)</TableCell>
+                                        <TableCell>Min Joueurs</TableCell>
+                                        <TableCell>Max Joueurs</TableCell>
+                                        <TableCell>Catégorie</TableCell>
+                                        <TableCell>Notice</TableCell>
+                                        <TableCell>Prototype ?</TableCell>
+                                        <TableCell> </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {gameList.data.map((row) => (
+                                        <TableRow key={row._id}>
+                                            <TableCell>{row.gameName}</TableCell>
+                                            <TableCell>{row.gameMinimumAge}</TableCell>
+                                            <TableCell>{row.gameDuration}</TableCell>
+                                            <TableCell>{row.gameMinimumPlayers}</TableCell>
+                                            <TableCell>{row.gameMaximumPlayers}</TableCell>
+                                            <TableCell>{row.gameType.gameTypeName}</TableCell>
+                                            <TableCell>{row.gameNotice}</TableCell>
+                                            <TableCell>{row.isPrototype ? 'Oui' : 'Non'}</TableCell>
                                             <TableCell>
                                                 <IconButton variant="outlined" color="primary" component={Link} to={`/game/${row._id}`}><Visibility /></IconButton>
                                                 <IconButton variant="outlined" color="primary" ><Create /></IconButton>
                                                 <IconButton variant="outlined" color="primary" ><Delete /></IconButton>
                                             </TableCell>
-                                        : <></>
-                                    }
-                                </TableRow>
-                            ))}
-                            </TableBody>
-                        </Table>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Grid>
                     </Grid>
-                </Grid>
-            )
+                )
+            } else {
+                return (
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}><h1>{editor.data.editorName}</h1></Grid>
+
+                        <Grid item xs={9}>
+                            <Typography><h3>Liste des jeux</h3></Typography>
+                            <Table aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Nom</TableCell>
+                                        <TableCell>Age Min</TableCell>
+                                        <TableCell>Durée (en min)</TableCell>
+                                        <TableCell>Min Joueurs</TableCell>
+                                        <TableCell>Max Joueurs</TableCell>
+                                        <TableCell>Catégorie</TableCell>
+                                        <TableCell>Notice</TableCell>
+                                        <TableCell>Prototype ?</TableCell>
+                                        <TableCell> </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {gameList.data.map((row) => (
+                                        <TableRow key={row._id}>
+                                            <TableCell>{row.gameName}</TableCell>
+                                            <TableCell>{row.gameMinimumAge}</TableCell>
+                                            <TableCell>{row.gameDuration}</TableCell>
+                                            <TableCell>{row.gameMinimumPlayers}</TableCell>
+                                            <TableCell>{row.gameMaximumPlayers}</TableCell>
+                                            <TableCell>{row.gameType.gameTypeName}</TableCell>
+                                            <TableCell>{row.gameNotice}</TableCell>
+                                            <TableCell>{row.isPrototype ? 'Oui' : 'Non'}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Grid>
+                    </Grid>
+                )
+            }
         }
         if(editor.loading) {
             return <Loading color={'lightblue'} type={'bubbles'} />
