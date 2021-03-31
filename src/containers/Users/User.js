f5dc7a21d35f49f5ee8fe5beb8833e87d0ad67ce
AxelCanton <axelcanton@hotmail.com>
2021-03-31 17:56:05 +0200

    user update

--- src/containers/Users/User.js
+++ src/containers/Users/User.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import {useSelector} from "react-redux";
+import React, { useState } from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {patchUser} from "../../actions/UserActions";
 
 const User = () => {
 
     const user = useSelector(state => state.User);
 
+    const initialUser = {
+        userName: "",
+        userPassword: "",
+    };
+
+    const [userState, setState] = useState(initialUser);
+    const dispatch = useDispatch();
+    const handleSubmit = () => {
+        dispatch(patchUser)
+    };
+
+    const handleChange = (event) => {
+        if(event.target) {
+            const { name, value } = event.target;
+            setState({ ...userState, [name]: value });
+        } 
+    };
 
     const showData = () => {
         console.log(user);
@@ -12,8 +30,19 @@ const User = () => {
             return (
                 <>
                     <h1>Mon Compte</h1>
-                    <p>{user.user.userInfo.userMail}</p>
-                    <p>{user.user.userInfo.userPassword}</p>
+                    {/* <p>{user.user.userInfo.userMail}</p> */}
+                    {/* <p>{user.user.userInfo.userPassword}</p> */}
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            Adresse mail :
+                            <input type="text" name="userName" value={userState.userName} onChange={handleChange}></input>
+                        </label>
+                        <label>
+                            Mot de passe :
+                            <input type="password" name="userPassword" value={userState.userPassword} onChange={handleChange}></input>
+                        </label>
+                        <input type="submit" text="Envoyer" value="Envoyer" />
+                    </form>
                 </>
             )
         }
--- src/reducers/User/AuthReducer.js
+++ src/reducers/User/AuthReducer.js
@@ -19,6 +19,11 @@ const UserListReducer = (state = DefaultState, action) => {
                 isLoggedIn: false,
                 user: null,
             };
+        case "USER_UPDATED_SUCCESS":
+            return {
+                ...state,
+                user: action.payload
+            }
         default:
             return state;
     }
